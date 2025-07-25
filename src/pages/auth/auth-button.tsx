d9df9436bfefbac623a34cc5b636bb5ee580aff0
Pabo Cervigon <pablocervigon@gmail.com>
2025-07-25 18:21:03 +0200

    selectors

--- src/pages/auth/auth-button.tsx
+++ src/pages/auth/auth-button.tsx
@@ -3,9 +3,10 @@ import { authLogout } from "../../store/actions";
 import { useAppDispatch, useAppSelector } from "../../store";
 import { logout } from "./service";
 import { Link } from "react-router";
+import { getIsLogged } from "../../store/selectors";
 
 function AuthButton() {
-  const isLogged = useAppSelector((state) => state.auth);
+  const isLogged = useAppSelector(getIsLogged);
   const dispatch = useAppDispatch();
   const handleLogoutClick = async () => {
     await logout();
--- src/pages/auth/require-auth.tsx
+++ src/pages/auth/require-auth.tsx
@@ -1,8 +1,9 @@
 import { Navigate, Outlet, useLocation } from "react-router";
 import { useAppSelector } from "../../store";
+import { getIsLogged } from "../../store/selectors";
 
 function RequireAuth() {
-  const isLogged = useAppSelector((state) => state.auth);
+  const isLogged = useAppSelector(getIsLogged);
   const location = useLocation();
   if (!isLogged) {
     return <Navigate to="/login" state={{ from: location.pathname }} replace />;
