6f010154b1241065aaf0b0fce9e288fc1580c73d
Pablo Cervigon <pablocervigon@gmail.com>
2025-08-24 19:34:41 +0200

    test thunks

--- src/store/actions.spec.ts
+++ src/store/actions.spec.ts
@@ -1,5 +1,5 @@
-import { authLoginPending, advertsLoadedFulfilled } from "./actions";
-
+import type { Credentials } from "../pages/auth/types";
+import { authLoginPending, advertsLoadedFulfilled, authLogin } from "./actions";
 describe("authLoginPending", () => {
   test('should return an  "auth/login/pending" action', () => {
     const expected = { type: "auth/login/pending" };
@@ -27,5 +27,65 @@ describe("advertsLoadedFulfilled", () => {
     const result = advertsLoadedFulfilled(adverts);
     expect(result).toEqual(expected);
     expect(result.payload).toHaveLength(2);
+  });
+});
+
+describe("authLogin", () => {
+  afterEach(() => {
+    dispatch.mockClear();
+    router.navigate.mockClear();
+  });
+
+  const credentials: Credentials = {
+    email: "er@test.com",
+    password: "1234",
+  };
+  const thunk = authLogin(credentials, true);
+
+  const dispatch = vi.fn();
+  const api = {
+    auth: {
+      login: vi.fn(),
+    },
+  };
+  const from = "/from";
+  const router = {
+    state: { location: { state: { from } } },
+    navigate: vi.fn(),
+  };
+
+  test("when login resolves", async () => {
+    api.auth.login = vi.fn().mockResolvedValue(undefined);
+    // @ts-expect-error: no need getState
+    await thunk(dispatch, undefined, { api, router });
+
+    expect(dispatch).toHaveBeenCalledTimes(2);
+    expect(dispatch).toHaveBeenNthCalledWith(1, { type: "auth/login/pending" });
+    expect(dispatch).toHaveBeenNthCalledWith(2, {
+      type: "auth/login/fulfilled",
+    });
+
+    expect(api.auth.login).toHaveBeenCalledWith(credentials, true);
+
+    expect(router.navigate).toHaveBeenCalledWith(from, { replace: true });
+  });
+
+  test("when login rejects", async () => {
+    const error = new Error("unauthorized");
+    api.auth.login = vi.fn().mockRejectedValue(error);
+
+    await expect(() =>
+      // @ts-expect-error: no need getState
+      thunk(dispatch, undefined, { api, router }),
+    ).rejects.toThrowError(error);
+
+    expect(dispatch).toHaveBeenCalledTimes(2);
+    expect(dispatch).toHaveBeenNthCalledWith(1, { type: "auth/login/pending" });
+    expect(dispatch).toHaveBeenNthCalledWith(2, {
+      type: "auth/login/rejected",
+      payload: error,
+    });
+
+    expect(router.navigate).not.toHaveBeenCalled();
   });
 });
