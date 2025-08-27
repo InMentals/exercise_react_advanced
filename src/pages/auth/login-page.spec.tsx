49f380c73b5999d158f7e94f2e52373c81d102f5
Pablo Cervigon <pablocervigon@gmail.com>
2025-08-27 17:23:28 +0200

    setup testing library and snapshot

---
+++ src/pages/auth/login-page.spec.tsx
@@ -0,0 +1,34 @@
+import { render } from "@testing-library/react";
+import LoginPage from "./login-page";
+import { Provider } from "react-redux";
+
+describe("LoginPage", () => {
+  const state = {
+    auth: false,
+    adverts: { loaded: false, data: [] },
+    ui: {
+      pending: false,
+      error: null,
+    },
+  };
+
+  const renderComponent = () =>
+    render(
+      <Provider
+        store={{
+          getState: () => state,
+          // @ts-expect-error: subscribe
+          subscribe: () => {},
+          // @ts-expect-error: dispatch
+          dispatch: () => {},
+        }}
+      >
+        <LoginPage />,
+      </Provider>,
+    );
+
+  test("should render", () => {
+    const { container } = renderComponent();
+    expect(container).toMatchSnapshot();
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,7 @@ export default defineConfig({
     coverage: {
       provider: "v8",
     },
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.ts",
   },
 });
