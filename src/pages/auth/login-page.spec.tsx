1db5c8d58b9808adacbdb651812df8286cb76002
Pablo Cervigon <pablocervigon@gmail.com>
2025-08-28 02:32:03 +0200

    test login page

--- src/pages/auth/login-page.spec.tsx
+++ src/pages/auth/login-page.spec.tsx
@@ -1,7 +1,10 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import LoginPage from "./login-page";
 import { Provider } from "react-redux";
 import { MemoryRouter } from "react-router-dom";
+import { authLogin } from "../../store/actions";
+
+vi.mock("../../store/actions");
 
 describe("LoginPage", () => {
   const state = {
@@ -33,5 +36,35 @@ describe("LoginPage", () => {
   test("should render", () => {
     const { container } = renderComponent();
     expect(container).toMatchSnapshot();
+  });
+
+  test("should dispatch login action", () => {
+    renderComponent();
+
+    const emailInput = screen.getByLabelText(/Email/);
+    const passwordInput = screen.getByLabelText(/Password/);
+    const rememberMeCheckbox = screen.getByRole("checkbox");
+
+    expect(rememberMeCheckbox).toBeChecked();
+
+    const button = screen.getByRole("button");
+
+    expect(button).toHaveTextContent("Log in");
+    expect(button).toBeDisabled();
+
+    fireEvent.change(emailInput, { target: { value: "user@example.com" } });
+    fireEvent.change(passwordInput, { target: { value: "1234" } });
+
+    expect(button).toBeEnabled();
+
+    fireEvent.click(button);
+
+    expect(authLogin).toHaveBeenCalledWith(
+      {
+        email: "user@example.com",
+        password: "1234",
+      },
+      true,
+    );
   });
 });
